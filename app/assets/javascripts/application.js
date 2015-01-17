56604bbc1a004331467dcc338c4879baacb52bc8
Jordan Singer <singer.jordan@gmail.com>
2015-01-17 04:25:26 -0500

    Material working on dashboard/profile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require angular
+//= require angular-animate
+//= require angular-aria
 //= require_tree .
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150117042228) do
+ActiveRecord::Schema.define(version: 20150117070415) do
 
   create_table "forms", force: true do |t|
     t.datetime "created_at"
