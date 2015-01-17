1d12d12423e779832ef67f5aba04900418121d30
ianrtracey <ianrtracey@gmail.com>
2015-01-17 14:49:07 -0700

    Form and Profile Converted

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,3 +1,12 @@
 class Form < ActiveRecord::Base
 	belongs_to :user
+	has_one :team
+	accepts_nested_attributes_for :team
+
+	# Validations
+	validates :projects, presence: true, length: { maximum: 500 }
+	validates :first_hackathon, presence: true
+	validates :why, presence: true
+	validates :user_id, presence: true
+	validates :code_of_conduct, presence: true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
 class Team < ActiveRecord::Base
 	has_many :users
+	belongs_to :form
 end
